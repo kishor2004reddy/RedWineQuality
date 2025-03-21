28eb434110a44e8421b3ae9c8aa425cf5c685241
Kishor Reddy <kishorreddy6407@gmail.com>
2025-03-21 15:41:06 +0530

    Integrated Data Validation With Data Transformation

--- src/pipeline/stage_3_data_transformation.py
+++ src/pipeline/stage_3_data_transformation.py
@@ -1,3 +1,5 @@
+import logging
+
 from src.config.configuration import ConfigurationManager
 from src.components.data_transformation import DataTransformation
 from src.logging import logger
@@ -10,10 +12,22 @@ class DataTransformationTrainingPipeline:
     @staticmethod
     def main():
         try:
-            config = ConfigurationManager()
-            data_transform_config = config.get_data_transformation_config()
-            data_transform = DataTransformation(config=data_transform_config)
-            data_transform.train_test_split()
+
+            with open("artifacts/data_validation/status.txt", "r") as file:
+                line1 = file.readline()
+                line2 = file.readline()
+                columns_validation_status = line1.split(" ")[-1].strip() == "True"
+                columns_dtype_validation_status = line2.split(" ")[-1].strip() == "True"
+
+            if all([columns_validation_status, columns_dtype_validation_status]):
+                config = ConfigurationManager()
+                data_transform_config = config.get_data_transformation_config()
+                data_transform = DataTransformation(config=data_transform_config)
+                data_transform.train_test_split()
+            else:
+                logging.error("Data is not matching with the schema")
+                raise ValueError("Data is not matching with the schema")
+
         except Exception as e:
             logger.exception(f"{e}")
             raise e
